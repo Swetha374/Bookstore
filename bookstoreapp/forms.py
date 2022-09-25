764de9f545c3bb839b7f2fa458c15375036ad135
swetha <swethasasi374@gmail.com>
2022-09-25 12:59:35 +0530

    Bookstore

--- bookstoreapp/forms.py
+++ bookstoreapp/forms.py
@@ -1,24 +1,39 @@
 from django import forms
 from django.contrib.auth.models import User
 from bookstoreapp.models import Books
+from django.contrib.auth.forms import UserCreationForm
 
-class RegistrationForm(forms.ModelForm):
+class RegistrationForm(UserCreationForm):
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    password2 =forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
     class Meta:
         model=User
-        fields=[
-            "first_name",
-            "last_name",
-            "email",
-            "username",
-            "password"
-        ]
+        fields=["first_name","last_name","username","email","password1","password2"]
         widgets={
-            "password":forms.PasswordInput(attrs={"class":"form-control"}),
             "email":forms.EmailInput(attrs={"class":"form-control"}),
             "first_name":forms.TextInput(attrs={"class":"form-control"}),
-            "last_name": forms.TextInput(attrs={"class": "form-control"}),
-            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name":forms.TextInput(attrs={"class":"form-control"}),
+            "username":forms.TextInput(attrs={"class":"form-control"}),
         }
+
+
+# class RegistrationForm(forms.ModelForm):
+#     class Meta:
+#         model=User
+#         fields=[
+#             "first_name",
+#             "last_name",
+#             "email",
+#             "username",
+#             "password"
+#         ]
+#         widgets={
+#             "password":forms.PasswordInput(attrs={"class":"form-control"}),
+#             "email":forms.EmailInput(attrs={"class":"form-control"}),
+#             "first_name":forms.TextInput(attrs={"class":"form-control"}),
+#             "last_name": forms.TextInput(attrs={"class": "form-control"}),
+#             "username": forms.TextInput(attrs={"class": "form-control"}),
+#         }
 
 class LoginForm(forms.Form):
     username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
